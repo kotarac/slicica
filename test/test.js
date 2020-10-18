812ec776c1ecce1c0bb6fae88ba901c328c4827c
kotarac <stipe@kotarac.net>
2020-10-18 16:21:44 +0200

    png tests

--- test/test.js
+++ test/test.js
@@ -2,8 +2,9 @@ const binaryParser = require('superagent-binary-parser')
 const express = require('express')
 const imageSize = require('image-size')
 const r = require('supertest')
-const slicica = require('../')
 const test = require('tape')
+
+const slicica = require('../')
 
 const app = express()
 const opts = {
@@ -45,26 +46,50 @@ test('invalid content type', function (t) {
   })
 })
 
-test('plain', function (t) {
+test('jpg plain', function (t) {
   get(app, '/image/wat.jpg', assert(t, 604, 404, 'jpg'))
 })
 
-test('height', function (t) {
+test('jpg height', function (t) {
   get(app, '/image/wat.jpg?h=100', assert(t, null, 100, 'jpg'))
 })
 
-test('width', function (t) {
+test('jpg width', function (t) {
   get(app, '/image/wat.jpg?w=100', assert(t, 100, null, 'jpg'))
 })
 
-test('width & height', function (t) {
+test('jpg width & height', function (t) {
   get(app, '/image/wat.jpg?w=100&h=100', assert(t, 100, 100, 'jpg'))
 })
 
-test('width & height & gravity', function (t) {
+test('jpg width & height & gravity', function (t) {
   get(app, '/image/wat.jpg?w=100&h=100&g=north', assert(t, 100, 100, 'jpg'))
 })
 
-test('width & height & max', function (t) {
+test('jpg width & height & max', function (t) {
   get(app, '/image/wat.jpg?w=1000&h=1000&max=1', assert(t, 604, 404, 'jpg'))
+})
+
+test('png plain', function (t) {
+  get(app, '/image/wat.png', assert(t, 604, 404, 'png'))
+})
+
+test('png height', function (t) {
+  get(app, '/image/wat.png?h=100', assert(t, null, 100, 'png'))
+})
+
+test('png width', function (t) {
+  get(app, '/image/wat.png?w=100', assert(t, 100, null, 'png'))
+})
+
+test('png width & height', function (t) {
+  get(app, '/image/wat.png?w=100&h=100', assert(t, 100, 100, 'png'))
+})
+
+test('png width & height & gravity', function (t) {
+  get(app, '/image/wat.png?w=100&h=100&g=north', assert(t, 100, 100, 'png'))
+})
+
+test('png width & height & max', function (t) {
+  get(app, '/image/wat.png?w=1000&h=1000&max=1', assert(t, 604, 404, 'png'))
 })
